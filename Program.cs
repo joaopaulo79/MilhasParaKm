c83c68a7d1c45277fb289e987fff7bc83ebe82f0
João Paulo <joaopdejesus79@gmail.com>
2021-09-22 11:27:36 -0300

    Programa Finalizado!

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MilhasParaKm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("==== Conversor de Milhas para Km ====");
+            Console.WriteLine();
+            Console.Write("Digite a medida em milhas: ");
+            Console.ResetColor();
+            string milhaDIG = Console.ReadLine();
+            double milhas = Convert.ToDouble(milhaDIG);
+            double km = milhas * 1.609;
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("== Conversão ==");
+            Console.ResetColor();
+            Console.WriteLine();
+            Console.WriteLine($"{milhas} milhas são {km:N1}km");
+            Console.WriteLine();
+        }
+    }
+}
